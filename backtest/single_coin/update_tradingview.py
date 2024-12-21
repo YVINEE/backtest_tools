4b370e0217c5851b63bf2d72076936e1445a4789
Yann <yvinee@gmail.com>
2024-12-21 07:51:06 +0100

    si Chrome est démarré alors on utilise la session existante

--- backtest/single_coin/update_tradingview.py
+++ backtest/single_coin/update_tradingview.py
@@ -1,4 +1,3 @@
-# %%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,6 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException
 from webdriver_manager.chrome import ChromeDriverManager
 import traceback
 from time import gmtime, strftime
@@ -19,6 +20,8 @@ import psutil
 import signal
 import mailtrap as mt
 from datetime import datetime, timedelta
+import socket
+import requests
 
 script_directory = os.path.abspath('')
 if os.name == 'nt':
@@ -31,6 +34,40 @@ else :
 backtest_path = os.path.join(envelope_db_path, 'week_backtesting100.db3')
 config_database_path = os.path.join(envelope_db_path, 'config.db3')
 pinescript_path = os.path.join(envelope_db_path, 'pinescript.txt')
+
+def check_chrome_debugging_session():
+    # Vérifier si le port 9222 est ouvert
+    def is_port_open(port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(('127.0.0.1', port))
+        sock.close()
+        return result == 0
+
+    # Vérifier si un processus Chrome avec l'option de débogage est en cours d'exécution
+    def chrome_process_running():
+        for proc in psutil.process_iter(['name', 'cmdline']):
+            if proc.info['name'] and 'chrome' in proc.info['name'].lower():
+                if proc.info['cmdline'] and '--remote-debugging-port=9222' in proc.info['cmdline']:
+                    return True
+        return False
+
+    # Tenter de se connecter à l'API de débogage
+    def test_debugging_connection():
+        try:
+            response = requests.get('http://localhost:9222/json/version', timeout=2)
+            return response.status_code == 200
+        except requests.RequestException:
+            return False
+
+    port_open = is_port_open(9222)
+    process_running = chrome_process_running()
+    connection_successful = test_debugging_connection()
+
+    print(f"Port 9222 ouvert : {port_open}")
+    print(f"Processus Chrome avec option de débogage en cours : {process_running}")
+    print(f"Connexion à l'API de débogage réussie : {connection_successful}")
+
+    return port_open and process_running and connection_successful
 
 try:
     with sqlite3.connect(config_database_path, 10, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as con:      
@@ -147,49 +184,61 @@ try:
 
 
     pair_list_to_study = sorted(list(set(matches) - set(pair_list_in_config)))
-    for proc in psutil.process_iter(['name', 'cmdline']):
-        try:
-            if proc.info['cmdline'] != None:
-                cmdline = ' '.join(map(str, proc.info['cmdline']))  # Convert each argument to string using map()
-                #print(cmdline)
-                if 'chrome' in cmdline :
-                    proc.send_signal(signal.SIGTERM) # Terminate the process
-                    print(f"Terminated process: {proc.pid}")
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass    
 
+    chrome_already_open = False
     options = webdriver.ChromeOptions()
-    options.add_argument("--password-store=basic")
-    options.add_argument("--start-maximized")
-    options.add_experimental_option(
-        "prefs",    
-        {
-            "credentials_enable_service": False,
-            "profile.password_manager_enabled": False,
-        },
-    )    
+    if check_chrome_debugging_session():
+        print("Une session Chrome avec le port de débogage 9222 est active.")
+        #on utilise la session chrome déjà ouverte        
+        options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        chrome_already_open = True
+    else:
+        print("Aucune session Chrome avec le port de débogage 9222 n'a été détectée.")
+        #on kill toutes les sessions Chrome
+        for proc in psutil.process_iter(['name', 'cmdline']):
+            try:
+                if proc.info['cmdline'] != None:
+                    cmdline = ' '.join(map(str, proc.info['cmdline']))  # Convert each argument to string using map()
+                    #print(cmdline)
+                    if 'chrome' in cmdline :
+                        proc.send_signal(signal.SIGTERM) # Terminate the process
+                        print(f"Terminated process: {proc.pid}")
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass    
+        options.add_argument("--password-store=basic")
+        options.add_argument("--start-maximized")
+        options.add_experimental_option(
+            "prefs",    
+            {
+                "credentials_enable_service": False,
+                "profile.password_manager_enabled": False,
+            },
+        )    
 
     browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
-    url = 'https://www.tradingview.com/accounts/signin/'
-    browser.get(url)
 
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.NAME, 'Email'))).click()
-    
-    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'id_username'))).send_keys('yvinee@protonmail.com')
+    if chrome_already_open == False:
+        #pas de session chrome déjà ouverte on se connecte à TradingView
+        url = 'https://www.tradingview.com/accounts/signin/'
+        browser.get(url)
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.NAME, 'Email'))).click()        
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'id_username'))).send_keys('yvinee@protonmail.com')
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'id_password'))).send_keys('Uar@ulnty86330')
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-overflow-tooltip-text='Sign in']"))).click()        
+        time.sleep(30)
 
-    WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'id_password'))).send_keys('Uar@ulnty86330')
-
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-overflow-tooltip-text='Sign in']"))).click()
-
-    
-    time.sleep(30)
     url='https://www.tradingview.com/chart'
     browser.get(url)
+
+    try:
+        accept_all = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-overflow-tooltip-text='Accept all ']"))
+        )
+        accept_all.click()
+    except TimeoutException:
+        print("accept_all not found")    
     
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-overflow-tooltip-text='Accept all ']"))).click() 
-
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Remove objects']"))).click()
-
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-name='remove-studies']"))).click()
 
     xpath = "//button[@aria-label = 'Open Pine Editor'][@data-active='false']"
@@ -201,8 +250,10 @@ try:
     else:
         print("Open Pine Editor is NOT found")
 
-    
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".view-lines.monaco-mouse-cursor-text"))).click()
+    time.sleep(2)
+    view_lines = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".view-lines.monaco-mouse-cursor-text")))
+    browser.execute_script("arguments[0].click();", view_lines)
+
     time.sleep(2)
     editor = browser.switch_to.active_element
     time.sleep(2)
@@ -213,7 +264,8 @@ try:
     ActionChains(browser).send_keys(Keys.RETURN).perform()
     time.sleep(4)
 
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".view-lines.monaco-mouse-cursor-text"))).click()
+    view_lines = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".view-lines.monaco-mouse-cursor-text")))
+    browser.execute_script("arguments[0].click();", view_lines)
 
     editor = browser.switch_to.active_element
 
@@ -332,12 +384,16 @@ try:
     else:
         print("Clear List is NOT found")
     
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Remove objects']"))).click()
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-name='remove-studies']"))).click()    
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "header-toolbar-save-load"))).click()
-    time.sleep(30)
-    browser.close()
-
+    if chrome_already_open == False:
+        #si chrome n'était pas déjà ouvert alors on le ferme en enlevant les stratégies et les objets et en sauvegardant préalablement
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Remove objects']"))).click()
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-name='remove-studies']"))).click()    
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "header-toolbar-save-load"))).click()
+        time.sleep(30)
+        browser.close()
+    else :
+        #on sauvegarde
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "header-toolbar-save-load"))).click()
 except Exception:
     print(traceback.format_exc())
     pass
